6a31bb85b60c6000b13da66bb44f663792805447
Daniel Carmona <daniel.carmona@masglobalconsulting.com>
2023-01-05 23:40:07 -0500

    Fixing naming in models

--- AuthenticationOAuth2Google.Domain/Models/User.cs
+++ AuthenticationOAuth2Google.Domain/Models/User.cs
@@ -13,7 +13,7 @@ namespace AuthenticationOAuth2Google.Domain.Models
 
         public string Email { get; set; }
 
-        public string Fullname { get; set; }
+        public string FullName { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
         public string AvatarUrl { get; set; }
--- AuthenticationOAuth2Google.Infrastructure/Context/Entities/UserEntity.cs
+++ AuthenticationOAuth2Google.Infrastructure/Context/Entities/UserEntity.cs
@@ -24,7 +24,7 @@ namespace AuthenticationOAuth2Google.Infrastructure.Context.Entities
 
         public string Email { get; set; }
 
-        public string Fullname { get; set; }
+        public string FullName { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
         public string AvatarUrl { get; set; }
