498c58974e77c11c707a78e02589884ec62a8abc
Daniel Carmona <daniel.carmona@masglobalconsulting.com>
2023-01-05 23:43:18 -0500

    Adding thread locking when user creates/login to prevent multiple request at the same time for the same user

--- AuthenticationOAuth2Google/Authentication/FirebaseAuthenticationHandler.cs
+++ AuthenticationOAuth2Google/Authentication/FirebaseAuthenticationHandler.cs
@@ -1,4 +1,5 @@
-﻿using AuthenticationOAuth2Google.Domain.Constants;
+﻿using AuthenticationOAuth2Google.Controllers;
+using AuthenticationOAuth2Google.Domain.Constants;
 using AuthenticationOAuth2Google.Domain.Models;
 using AuthenticationOAuth2Google.Infrastructure.Context.Entities;
 using AuthenticationOAuth2Google.Infrastructure.Interfaces;
@@ -17,6 +18,7 @@ namespace AuthenticationOAuth2Google.Authentication
         private const string BEARER_PREFIX = "Bearer ";
         private readonly FirebaseApp _firebaseApp;
         private readonly IMongoDBRepository<UserEntity> _mongoDBRepository;
+        static SemaphoreSlim semaphoreSlim = new SemaphoreSlim(1, 1);
 
         public FirebaseAuthenticationHandler(
             IOptionsMonitor<AuthenticationSchemeOptions> options,
@@ -44,33 +46,62 @@ namespace AuthenticationOAuth2Google.Authentication
                 return AuthenticateResult.Fail("Invalid scheme");
             }
 
+            string authType = string.Empty;
+            if (Context.Request.Headers.ContainsKey("x-auth-type")) 
+            {
+                authType = Context.Request.Headers["x-auth-type"]!;
+            }
+
             string token = bearerToken.Substring(BEARER_PREFIX.Length);
 
             try
             {
                 FirebaseToken firebaseToken = await FirebaseAuth.GetAuth(_firebaseApp).VerifyIdTokenAsync(token);
-
-                // TODO: Check user against the database
-                var user = (await _mongoDBRepository.GetAsync()).FirstOrDefault(x => x.Email == firebaseToken.Claims["email"].ToString());
-                if (user == null) 
+                //Instantiate a Singleton of the Semaphore with a value of 1. This means that only 1 thread can be granted access at a time.
+                //Asynchronously wait to enter the Semaphore. If no-one has been granted access to the Semaphore, code execution will proceed, otherwise this thread waits here until the semaphore is released 
+                // Only lock the thread when the user is trying to login
+                if (authType != "") 
+                    await semaphoreSlim.WaitAsync();
+                
+                try
                 {
-                    var userEntity = new UserEntity()
+                    var user = (await _mongoDBRepository.GetAsync()).FirstOrDefault(x => x.Email == firebaseToken.Claims["email"].ToString());
+                    if (user == null)
                     {
-                        CreationDate = DateTime.Now,
-                        Enabled = true,
-                        Email = firebaseToken.Claims["email"].ToString()!,
-                        Fullname = firebaseToken.Claims["name"].ToString()!,
-                        Role = ClaimsConstants.DEFAULT_ROLE,
-                        AvatarUrl = firebaseToken.Claims["picture"].ToString()!,
-                        OAuthId = firebaseToken.Claims["user_id"].ToString()!
-                    };
-                    user = await _mongoDBRepository.CreateAsync(userEntity);
+                        AUTH_TYPE parsedAuthType;
+                        if (!Enum.TryParse(authType, true, out parsedAuthType))
+                        {
+                            return AuthenticateResult.Fail("Authentication type not valid");
+                        }
+
+                        var userEntity = new UserEntity()
+                        {
+                            CreationDate = DateTime.Now,
+                            Enabled = true,
+                            Email = firebaseToken.Claims["email"].ToString()!,
+                            FullName = firebaseToken.Claims["name"].ToString()!,
+                            Role = ClaimsConstants.DEFAULT_ROLE,
+                            AvatarUrl = firebaseToken.Claims["picture"].ToString()!,
+                            OAuthId = firebaseToken.Claims["user_id"].ToString()!,
+                            AuthType = parsedAuthType
+                        };
+                        
+                        user = await _mongoDBRepository.CreateAsync(userEntity);
+                    }
+
+                    return AuthenticateResult.Success(new AuthenticationTicket(new ClaimsPrincipal(new List<ClaimsIdentity>()
+                    {
+                        new ClaimsIdentity(ToClaims(firebaseToken.Claims, user), nameof(FirebaseAuthenticationHandler))
+                    }), JwtBearerDefaults.AuthenticationScheme));
+                }
+                finally
+                {
+                    //When the task is ready, release the semaphore. It is vital to ALWAYS release the semaphore when we are ready, or else we will end up with a Semaphore that is forever locked.
+                    //This is why it is important to do the Release within a try...finally clause; program execution may crash or take a different path, this way you are guaranteed execution
+                    semaphoreSlim.Release();
                 }
 
-                return AuthenticateResult.Success(new AuthenticationTicket(new ClaimsPrincipal(new List<ClaimsIdentity>()
-            {
-                new ClaimsIdentity(ToClaims(firebaseToken.Claims, user), nameof(FirebaseAuthenticationHandler))
-            }), JwtBearerDefaults.AuthenticationScheme));
+            
             }
             catch (Exception ex)
             {
@@ -82,9 +113,10 @@ namespace AuthenticationOAuth2Google.Authentication
         {
             return new List<Claim> 
             { 
-                new Claim("id", user.OAuthId!),
+                new Claim("OAuthId", user.OAuthId!),
+                new Claim("internal_id", user.Id!),
                 new Claim(ClaimTypes.Email, user.Email!),
-                new Claim(ClaimTypes.Name, user.Fullname!),
+                new Claim(ClaimTypes.Name, user.FullName!),
                 new Claim(ClaimTypes.Role, user.Role)
             };
         }
